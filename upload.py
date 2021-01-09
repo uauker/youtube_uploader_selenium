d347ab42e62af15a41a4ed4ec24a8dae5b07ad31
Uauker <uauker@gmail.com>
2021-01-09 16:36:53 -0300

    added thumb in parameter

--- upload.py
+++ upload.py
@@ -3,8 +3,8 @@ from youtube_uploader_selenium import YouTubeUploader
 from typing import Optional
 
 
-def main(video_path: str, metadata_path: Optional[str] = None):
-    uploader = YouTubeUploader(video_path, metadata_path)
+def main(video_path: str, thumb_path: Optional[str] = None, metadata_path: Optional[str] = None):
+    uploader = YouTubeUploader(video_path, thumb_path, metadata_path)
     was_video_uploaded, video_id = uploader.upload()
     assert was_video_uploaded
 
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("--video",
                         help='Path to the video file',
                         required=True)
+    parser.add_argument("--thumb", help='Path to the thumb file')
     parser.add_argument("--meta", help='Path to the JSON file with metadata')
     args = parser.parse_args()
-    main(args.video, args.meta)
+    main(args.video, args.thumb, args.meta)
